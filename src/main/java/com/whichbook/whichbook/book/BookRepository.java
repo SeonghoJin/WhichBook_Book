a6db46e9ea491f6bddb491c65060280512ea0b50
SeonghoJin <seongho4996@naver.com>
2021-01-06 14:50:46 +0900

    page을 위한 쿼리 추가

--- src/main/java/com/whichbook/whichbook/book/BookRepository.java
+++ src/main/java/com/whichbook/whichbook/book/BookRepository.java
@@ -1,12 +1,19 @@
 package com.whichbook.whichbook.book;
 
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     boolean existsByIsbn(String Isbn);
     List<Book> findAllByTitleContains(String title);
+
+    @Query("select book From Book book where book.title like %:title% and book.id > :id order by book.id")
+    List<Book> findAllByTitleUsingPage(@Param("title") String title, @Param("id") Long id, Pageable pageable);
 }
